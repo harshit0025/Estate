8269dfefe05f53b91ab422c3135100f784b9d112
Harshit <harshityadav.0025@gmail.com>
2024-08-05 11:15:58 +0530

    Complete sign-up page functionality

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cors from 'cors';
 dotenv.config({ path: '../.env'});
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -13,6 +14,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.listen(3000, ()=>{
